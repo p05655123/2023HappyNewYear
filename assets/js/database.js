2e75405656b392e1cc2933fe25f38759bc4aba1a
林利印 <>
2023-01-05 02:26:24 +0800

    addGame2

--- assets/js/database.js
+++ assets/js/database.js
@@ -27,30 +27,70 @@ googleAuthProvider.addScope('profile');
 googleAuthProvider.addScope('email');
 auth.languageCode = 'it';
 
+async function uploadData(userName, userUid) {
+  try {
+    await setDoc(doc(db, "users", userUid), {
+      name: userName,
+      answer1: document.getElementById("answer1").value,
+      answer2: document.getElementById("answer2").value,
+      answer3: document.getElementById("answer3").value
+    }, { merge: true });
+    alert('下注成功！請注意，下注總數務必超過500印幣，如果沒有超過，利印會通知你進行更改，除夕當日12:00前皆可更改答案 ！');
+  } catch (err) {
+    console.error("Error: ", err);
+    alert('檔案沒有上傳成功，請通知利印 ！');
+  }
+}
+
+
+async function uploadData2(userName, userUid) {
+  try {
+    await setDoc(doc(db, "users", userUid), {
+      name: userName,
+      game2_answer1: document.getElementById("game2_answer1").value,
+      game2_answer2: document.getElementById("game2_answer2").value,
+      game2_answer3: document.getElementById("game2_answer3").value,
+      game2_answer4: document.getElementById("game2_answer4").value,
+      game2_answer5: document.getElementById("game2_answer5").value
+    }, { merge: true });
+    alert('上傳成功 ！除夕當日12:00前皆可更改答案 ！');
+  } catch (err) {
+    console.error("Error: ", err);
+    alert('檔案沒有上傳成功，請通知利印 ！');
+  }
+}
+
 window.uploadAnswer = async function uploadAnswer(){
   onAuthStateChanged(auth, user => {
     if(user){
       if(document.getElementById("answer1").value === "" || document.getElementById("answer2").value === "" || document.getElementById("answer3").value === ""){
         alert('您還有未下注的內容 ！');
       }else{
-        var yes = confirm('請注意，下注總數務必超過500印幣，如果沒有超過，利印會通知你進行更改，你確定要送出下注內容嗎？');
+        var yes = confirm('你確定要送出下注內容嗎？(請務必等到「下注成功」訊息彈出，否則檔案無法上傳)');
         if (yes) {
           var userName = user.displayName;
           var userUid = user.uid;
-          try {
-            setDoc(doc(db, "users", userUid), {
-              name: userName,
-              answer1: document.getElementById("answer1").value,
-              answer2: document.getElementById("answer2").value,
-              answer3: document.getElementById("answer3").value
-            }, { merge: true });
-          } catch (err) {
-            console.error("Error: ", err);
-          }
-          const unsub = onSnapshot(doc(db, "users", userUid), (doc) => {
-            console.log(doc.data());
-            alert('送出成功，除夕當日12:00前皆可更改您的答案！');
-          });
+          uploadData(userName, userUid);
+        }
+      }
+    }else{
+      alert('您還尚未登入，請登入後再進行下注 ！');
+    }
+  })
+}
+
+window.uploadAnswer2 = async function uploadAnswer2(){
+  onAuthStateChanged(auth, user => {
+    if(user){
+      if(document.getElementById("game2_answer1").value === "" || document.getElementById("game2_answer2").value === "" || document.getElementById("game2_answer3").value === ""
+      || document.getElementById("game2_answer4").value === "" || document.getElementById("game2_answer5").value === "" ){
+        alert('您還有未填答的內容 ！');
+      }else{
+        var yes = confirm('你確定要送出答案嗎？(請務必等到「上傳成功」訊息彈出，否則檔案無法上傳)');
+        if (yes) {
+          var userName = user.displayName;
+          var userUid = user.uid;
+          uploadData2(userName, userUid);
         }
       }
     }else{
@@ -76,13 +116,17 @@ window.logOut = async function logOut(){
   })
 }
 
+window.notOpen = async function notOpen(){
+  alert("開發中...敬請期待！");
+} 
+
 window.mainPage = async function mainPage(){
   onAuthStateChanged(auth, user => {
     if(user){
       var userUid = user.uid;
       try {
         getDoc(doc(db, "users", userUid)).then(docSnap => {
-          if (docSnap.exists()) {
+          if (docSnap.exists() &&  docSnap.data().answer1 != undefined) {
             document.getElementById("answer1").value = docSnap.data().answer1;
             document.getElementById("answer2").value = docSnap.data().answer2;
             document.getElementById("answer3").value = docSnap.data().answer3;
@@ -95,6 +139,33 @@ window.mainPage = async function mainPage(){
       document.getElementById("answer1").value = "";
       document.getElementById("answer2").value = "";
       document.getElementById("answer3").value = "";
+    }
+  })
+}
+
+window.game2Page = async function game2Page(){
+  onAuthStateChanged(auth, user => {
+    if(user){
+      var userUid = user.uid;
+      try {
+        getDoc(doc(db, "users", userUid)).then(docSnap => {
+          if (docSnap.exists() && docSnap.data().game2_answer1 != undefined ) {
+            document.getElementById("game2_answer1").value = docSnap.data().game2_answer1;
+            document.getElementById("game2_answer2").value = docSnap.data().game2_answer2;
+            document.getElementById("game2_answer3").value = docSnap.data().game2_answer3;
+            document.getElementById("game2_answer4").value = docSnap.data().game2_answer4;
+            document.getElementById("game2_answer5").value = docSnap.data().game2_answer5;
+          }
+        })
+      } catch (err) {
+        console.error("Error: ", err);
+      }
+    }else{
+      document.getElementById("game2_answer1").value = "";
+      document.getElementById("game2_answer2").value = "";
+      document.getElementById("game2_answer3").value = "";
+      document.getElementById("game2_answer4").value = "";
+      document.getElementById("game2_answer5").value = "";
     }
   })
 }
